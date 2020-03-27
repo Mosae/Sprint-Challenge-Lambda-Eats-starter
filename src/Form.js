e4136ed9d07ed1f81962593a80f15231d152d476
mosaelitsoane@gmail.com <mosaelitsoane@Mosaes-MacBook-Pro.local>
2020-03-27 11:57:09 -0500

    set up validate change function + gave name input value

--- src/Form.js
+++ src/Form.js
@@ -17,7 +17,7 @@ export default function Form() {
 		instructions: ''
 	});
 	//error state
-	const [error, setError] = useState({
+	const [errors, setErrors] = useState({
 		name: '',
 		size: '',
 		toppings: '',
@@ -33,11 +33,29 @@ export default function Form() {
 		});
 	}, [formState]);
 
+	const validateChange = e => {
+		yup
+			.reach(formSchema, e.target.name)
+			.validate(e.target.value)
+			.then(valid => {
+				setErrors({
+					...errors,
+					[e.target.name]: ''
+				});
+			})
+			.catch(err => {
+				setErrors({
+					...errors,
+					[e.target.name]: err.errors
+				});
+			});
+	};
+
 	return (
 		<form>
 			<label htmlFor="name">
 				Name:
-				<input id="name" type="text" name="name" />
+				<input id="name" type="text" name="name" value={formState.name} />
 			</label>
 			<label htmlFor="size">
 				What pizza size would you like?
