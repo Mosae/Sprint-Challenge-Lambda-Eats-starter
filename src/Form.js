485b7249e2b81796d29ce58999971d376798325b
mosaelitsoane@gmail.com <mosaelitsoane@Mosaes-MacBook-Pro.local>
2020-03-27 11:16:14 -0500

    Created dropdown for pizza size

--- src/Form.js
+++ src/Form.js
@@ -6,11 +6,24 @@ const formSchema = yup.object().shape({
 });
 
 export default function Form() {
+	const [formState, setFormState] = useState({
+		name: ''
+	});
 	return (
 		<form>
 			<label htmlFor="name">
 				Name:
 				<input id="name" type="text" name="name" />
+			</label>
+
+			<label htmlFor="pizza-size">
+				What pizza size would you like?
+				<select id="pizza-size" name="pizza-size">
+					<option value="small">Small</option>
+					<option value="medium"> Medium</option>
+					<option value="large"> Large</option>
+					<option value="xlarge"> Extra Large</option>
+				</select>
 			</label>
 		</form>
 	);
