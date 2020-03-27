08e93f272e730c21fff7b3ed77cef4843f6b0010
mosaelitsoane@gmail.com <mosaelitsoane@Mosaes-MacBook-Pro.local>
2020-03-27 11:06:00 -0500

    imported yup and declared formschema

--- src/Form.js
+++ src/Form.js
@@ -1,11 +1,16 @@
 import React, { useState, useEffect } from 'react';
+import * as yup from 'yup';
+
+const formSchema = yup.object().shape({
+	name: yup.string().required('Name is required')
+});
 
 export default function Form() {
 	return (
 		<form>
 			<label htmlFor="name">
 				Name:
-				<input type="text" name="name" />
+				<input id="name" type="text" name="name" />
 			</label>
 		</form>
 	);
