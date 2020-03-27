824266f072df229bc1740ddcf369b4688bf7d8dc
mosaelitsoane@gmail.com <mosaelitsoane@Mosaes-MacBook-Pro.local>
2020-03-27 13:13:09 -0500

    Test MVP done

--- cypress/integration/form_test.js
+++ cypress/integration/form_test.js
@@ -3,6 +3,9 @@ describe('Testing our form inputs', function() {
 		cy.visit('http://localhost:3000/pizza');
 	});
 	it('Adds text to inputs and submits form', function() {
+		cy.get('input[name="name"]')
+			.type('Mosae')
+			.should('have.value', 'Mosae');
 		cy.get('textarea')
 			.type('Leave at the door')
 			.should('have.value', 'Leave at the door');
